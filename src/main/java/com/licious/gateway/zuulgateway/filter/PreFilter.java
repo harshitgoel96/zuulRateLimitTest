b37012d94903818934ae96f20a34577ef9859ea5
SHRAYAN SANTRA <shrayan@licious.com>
2021-06-02 12:42:47 +0530

    commit

--- src/main/java/com/licious/gateway/zuulgateway/filter/PreFilter.java
+++ src/main/java/com/licious/gateway/zuulgateway/filter/PreFilter.java
@@ -3,7 +3,12 @@ package com.licious.gateway.zuulgateway.filter;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.exception.ZuulException;
 import com.netflix.zuul.context.RequestContext;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.binary.StringUtils;
+import org.apache.http.HttpHeaders;
+
 import javax.servlet.http.HttpServletRequest;
+import java.nio.charset.Charset;
 
 public class PreFilter extends ZuulFilter {
     @Override
@@ -22,11 +27,12 @@ public class PreFilter extends ZuulFilter {
     }
 
     @Override
-    public Object run() throws ZuulException {
-        RequestContext ctx = RequestContext.getCurrentContext();
-        HttpServletRequest request = ctx.getRequest();
-
-        System.out.println("Request Method : " + request.getMethod() + " Request URL : " + request.getRequestURL().toString());
+    public Object run()  {
+//        RequestContext ctx = RequestContext.getCurrentContext();
+//        String auth = "user" + ":" + "password";
+//        byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("ISO-8859-1")));
+//        String authValue = "Basic " + new String(encodedAuth);
+//        ctx.addZuulRequestHeader(HttpHeaders.AUTHORIZATION, authValue);
         return null;
     }
 }
